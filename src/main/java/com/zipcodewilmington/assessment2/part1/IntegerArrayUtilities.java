abedb410c60c8a7fdd051260d1a2dbf2bf29bac1
AngelicaSantos <angelica.santos2113@gmail.com>
2019-06-10 10:45:54 -0400

    part1

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,19 +1,36 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+
+        return true;
     }
 
     public Integer[] range(int start, int stop) {
+
+
         return null;
+
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+
+        int sum = 0;
+        for (int i =0; i<2; i++) {
+            sum += array [i];
+        }
+        return sum;
     }
 
+
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        int product = 1;
+        for (int i =1; i<=array.length-1; i++) {
+            product *= array [i];
+        }
+        return product;
     }
 }
